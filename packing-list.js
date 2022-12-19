a3d26bad77ff14e90306e3592cca3f5b61bdea4d
kimiakalantari <kimiakalantari97@gmail.com>
2022-12-19 15:44:41 -0500

    working to make a list of arrays items with different methods

---
+++ packing-list.js
@@ -0,0 +1,29 @@
+/*
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+*/
+
+const packingLists = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+/*
+console.log("Kitchen stuff to pack:");
+for (let packingList of packingLists) {
+  console.log(packingList);
+}
+*/
+
+/*
+console.log("Kitchen stuff to pack:");
+packingLists.forEach(function(item){
+  console.log(item)
+})
+*/
+
+console.log("Kitchen stuff to pack:");
+var list = packingLists.map(function(item){
+  console.log(item)
+})
