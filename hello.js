9133fc56376d519d4712e0e7ac449dc224873fad
kimiakalantari <kimiakalantari97@gmail.com>
2022-12-18 23:20:11 -0500

    more trials

--- hello.js
+++ hello.js
@@ -5,7 +5,20 @@ const sayHello  = function () {
 
 sayHello();
 */
+
+/*
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 sayHello("Caliban");
+*/
+/*
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+*/
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+console.log(returnSayHello('John'));
